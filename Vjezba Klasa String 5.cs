f33e91837b3000a86ff158bfdf65d4d03d3d0176
MichaelSchmitz02 <152979941+MichaelSchmitz02@users.noreply.github.com>
2024-01-16 12:47:04 +0100

    Add files via upload

---
+++ Vjezba Klasa String 5.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System;
+
+class Program
+{
+    static void Main()
+    { 
+        Console.Write("Unesite cijeli broj i decimalni broj odvojeni razmakom: ");
+        string unos = Console.ReadLine();
+       
+        string[] dijelovi = unos.Split(' ');
+
+        if (dijelovi.Length == 2 && int.TryParse(dijelovi[0], out int cijeliBroj) && double.TryParse(dijelovi[1], out double decimalniBroj))
+        {
+            Console.WriteLine("Uneseni cijeli broj: " + cijeliBroj);
+            Console.WriteLine("Uneseni decimalni broj: " + decimalniBroj);
+        }
+        else
+        {
+            Console.WriteLine("Pogrešan unos. Molimo unesite ispravne vrijednosti.");
+        }
+    }
+}
